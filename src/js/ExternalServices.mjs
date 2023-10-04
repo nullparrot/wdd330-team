8e805c401dab4752567a7a9124d031f06964ac8f
Jhonn Duarte <johnduarte7>
2023-10-04 19:11:27 -0300

    Refactoring

--- src/js/ExternalServices.mjs
+++ src/js/ExternalServices.mjs
@@ -9,7 +9,7 @@ function convertToJson(res) {
   }
 }
 
-export default class ProductData {
+export default class ExternalServices {
   async getData(category) {
     const response = await fetch(baseURL + `products/search/${category}`);
     const data = await convertToJson(response);
