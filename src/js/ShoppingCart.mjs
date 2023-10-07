c4482e6406e3feaabe03ec91b1bd7200f0475f10
nullparrot <github@weist.dev>
2023-10-07 11:02:00 -0700

    Cart items link to product page

--- src/js/ShoppingCart.mjs
+++ src/js/ShoppingCart.mjs
@@ -2,13 +2,13 @@ import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 
 function cartItemTemplate(item) {
    return `<li class="cart-card divider">
-    <a href="#" class="cart-card__image">
+    <a href="../product_pages/index.html?product=${item.Id}" class="cart-card__image">
       <img
         src="${item.Images.PrimaryMedium}"
         alt="${item.Name}"
       />
     </a>
-    <a href="#">
+    <a href="../product_pages/index.html?product=${item.Id}">
       <h2 class="card__name">${item.Name}</h2>
     </a>
     <p class="cart-card__color">${item.Colors[0].ColorName}</p>
